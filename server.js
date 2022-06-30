859fe6e893966444ba818bd7bce095122eb6dda2
Kadika <dryan10101@gmail.com>
2022-06-29 19:00:34 -0700

    changed routes folder to be named controllers

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
 
