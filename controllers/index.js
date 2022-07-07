00145e7a9eae9e53ba1a3b1ccdf430a0283cbfe9
Kadika <dryan10101@gmail.com>
2022-07-06 21:03:01 -0700

    all current handlebars files now have a route connected fully through controllers index js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const errorRoutes = require('./errorRoutes');
 const landingRoutes = require('./landingRoutes');
+const loadingRoutes = require('./loadingRoutes');
 const loginRoutes = require('./loginRoutes');
+const profileRoutes = require('./profileRoutes');
+const resultsRoutes = require('./resultsRoutes');
+const searchRoutes = require('./searchRoutes');
 
-router.use('/', landingRoutes);
 router.use('/api', apiRoutes);
+router.use('/error', errorRoutes);
+router.use('/', landingRoutes);
+router.use('/loading', loadingRoutes);
 router.use('/login', loginRoutes);
+router.use('/profile', profileRoutes);
+router.use('/results', resultsRoutes);
+router.use('/search', searchRoutes);
 
 module.exports = router;
