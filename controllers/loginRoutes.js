6c7e45de885fda6b1113aba8da1a2b715b82a862
David Rios <david.riostech@outlook.com>
2022-07-08 12:44:10 -0700

    Fixed Login.handlebars page layout, added signup.handlebars page and test route for it to test functionality still need styling. Added and postioned and styled profile page, still need more styling changes but basic layout complete.

--- controllers/loginRoutes.js
+++ controllers/loginRoutes.js
@@ -4,4 +4,9 @@ router.get('/', async(req, res) => {
     res.render('login');
 })
 
+// TEST for David R's Sign up handlebars
+router.get('/signup', async (req, res) => {
+    res.render('signup');
+})
+
 module.exports = router;
