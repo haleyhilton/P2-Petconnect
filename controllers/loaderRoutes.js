95d9c219a885c89889b2d81ae2536310415b9a92
Kadika <dryan10101@gmail.com>
2022-07-09 11:14:58 -0700

    loader route adjustmant

--- controllers/loaderRoutes.js
+++ controllers/loaderRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', async(req, res) => {
-    res.render('loader');
+    res.render('loader', {layout: 'main2'});
 })
 
 module.exports = router;
