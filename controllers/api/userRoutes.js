f63d90b0b7d1b92cdb7c80b83e19d483e38a6247
Kadika <dryan10101@gmail.com>
2022-07-09 11:00:02 -0700

    logout route setup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -42,4 +42,15 @@ router.post('/login', async (req, res) => {
       }
 });
 
+//route for logging out
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+});
+
 module.exports = router;
