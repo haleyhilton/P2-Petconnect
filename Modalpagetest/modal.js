ed8b658b905e6f12b11af5893f1ddf5b71eac6c2
NathanStein1 <nathanstein2@gmail.com>
2022-07-09 13:14:14 -0700

    template literal for modal

--- Modalpagetest/modal.js
+++ Modalpagetest/modal.js
@@ -1,8 +1,44 @@
+let dogButton = document.querySelectorAll('.dogNameOf')
+let invisibleModal = document.querySelector('.blankModal')
+
+// Let eventID = event.target.datatype
+// Datatype will be {{userid}}?
+
+
+function dogModal(){
+invisibleModal.textContent = `<div>
+<a class="nav-link conB" data-toggle="modal" data-target="#exampleModal"><button>Modal Click</button></a>
+<div id="myModal" class="modal">
+  <div class="modal-ccontent">
+    <span class="close">&times;</span>
+    <form class="formP">
+      <fieldset>
+
+        <div class="form-group">
+          <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+            placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">${{useriddogphoto}}</small>
+        </div>
+
+        <div class="form-group">
+          <label for="exampleTextarea" class="form-label mt-4">${{dogdetail}}</label>
+          <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </fieldset>
+    </form>
+  </div>
+
+</div>
+</div>
+<script src="modal.js"></script>`
+}
+
 // CODE FOR MODAL
 let modal = document.querySelector('.modal')
 let conB = document.querySelector('.conB')
-
-
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
