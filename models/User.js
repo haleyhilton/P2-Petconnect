79e6b042902e1165cc21644a04d1832e0f6bf858
Kadika <dryan10101@gmail.com>
2022-07-09 10:48:13 -0700

    login route setup

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
