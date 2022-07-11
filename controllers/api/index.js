6a89e6708237c237312c81167cbc939b00342d64
Kadika <dryan10101@gmail.com>
2022-07-11 13:24:08 -0700

    added sess routes to api index js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const dogRoutes = require('./dogRoutes');
 const userRoutes = require('./userRoutes');
 const uploadRoutes = require('./imageRoutes');
 const mediaRoutes = require('./mediaRoutes');
+const sessRoutes = require('./sessRoutes');
 
 router.use('/dogs', dogRoutes);
 router.use('/users', userRoutes);
 router.use('/upload', uploadRoutes);
-router.use('/media', mediaRoutes)
+router.use('/media', mediaRoutes);
+router.use('/sess', sessRoutes);
 
 module.exports = router;
