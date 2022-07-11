12372343728eb6f2b7b0c7845ab0e691d391c031
Tater <haleymariehilton@gmail.com>
2022-07-10 17:54:17 -0700

    changed images and modals from Landing page to an array using back ends work on landingRoutes.js to create more dynamically and avoid repetition.

--- controllers/landingRoutes.js
+++ controllers/landingRoutes.js
@@ -3,7 +3,18 @@ const router = require('express').Router();
 
 //Haley landing.handlebars routes for testing purposes
 router.get('/', async(req, res) => {
-    res.render('landing');
+    const icons = [
+        {image: "/images/beagle.png", modal: "modal1"},
+        {image: "/images/bulldog.png", modal: "modal2"},
+        {image: "/images/corgi.png", modal: "modal3"},
+        {image: "/images/frenchy.png", modal: "modal4"},
+        {image: "/images/german-s.png", modal: "modal5"},
+        {image: "/images/golden.png", modal: "modal6"},
+        {image: "/images/lab.png", modal: "modal7"},
+        {image: "/images/poodle.png", modal: "modal8"},
+        {image: "/images/rotty.png", modal: "modal9"},
+        ]
+    res.render('landing', {icons});
 })
 
 
