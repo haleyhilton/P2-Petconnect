4ad3b7e2b4cde8c2827c5af4c2721329a993ed87
Kadika <dryan10101@gmail.com>
2022-07-11 12:38:59 -0700

    profile button in header modal now loads profile page and profile page gets user_id and logged_in passed to it

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 router.get('/', async(req, res) => {
-    res.render('profile');
+    res.render('profile', {
+        logged_in: req.session.logged_in,
+        user_id: req.session.user_id,
+    });
 })
 
 module.exports = router;
