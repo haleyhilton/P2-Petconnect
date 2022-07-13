2ffd10ef74b71b6bd1b582aae57c732fc3012226
josh jankowski <joshjankowski@gmail.com>
2022-07-12 20:58:33 -0700

    damn

--- public/js/search.js
+++ public/js/search.js
@@ -1,14 +1,14 @@
-const search = querySelector('#search-input').value;
-const mySearchButton = querySelector('search-button');
+// const search = document.querySelector('#search-input').value.trim();
+// const mySearchButton = document.querySelector('#search-button');
 
-const dogSearchURL = "/results"
+// const dogSearchURL = "/results"
 
-async function dogSearch() {
-    const dogSearch = await fetch(dogSearchURL, {
-        method: "GET",
-      })
-        .then((response) => response.json())
-        .then((response) => console.log(response));
-}
+// async function dogSearch() {
+//     const dogSearch = await fetch(dogSearchURL, {
+//         method: "GET",
+//       })
+//         .then((response) => response.json())
+//         .then((response) => console.log(response));
+// }
 
-mySearchButton.addEventLister("click", dogSearch);
+// mySearchButton.addEventListener("click", dogSearch);
