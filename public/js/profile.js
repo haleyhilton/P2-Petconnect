7fadcf8fc6b8463b3175d4d7c8f3183a7fec9953
David Rios <david.riostech@outlook.com>
2022-07-12 22:17:05 -0700

    fixed styling of Profile page. Still need to fix modal styling and a few more styling for profile page.

--- public/js/profile.js
+++ public/js/profile.js
@@ -17,23 +17,3 @@ const profileHandler = async () => {
 
   profileHandler();
 
-<<<<<<< HEAD
-=======
-  const dogHandler = async () => {
-    const fetchURL = "/api/media/dog"
-    const response = await fetch(fetchURL, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    }).then(response => {
-      return response.json();
-    }).then(response => {
-      // console.log(response[0].picture_url)
-      // const newImage = document.createElement('img');
-      dogPicture.src = response[0].picture_url
-      // profilePicture.appendChild(newImage)
-      console.log(response[0].picture_url, "hello")
-    });
-  };
-
-  dogHandler();
->>>>>>> 1c980383c54b16eb42e84b5f5979eee7d0e3b470
